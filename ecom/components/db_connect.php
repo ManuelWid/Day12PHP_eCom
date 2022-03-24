60af56e412c83a5369d08f5050864ae80469110d
ManuelWid <manuel_widner@hotmail.com>
2022-03-24 10:03:42 +0100

    add dbconnect options

--- ecom/components/db_connect.php
+++ ecom/components/db_connect.php
@@ -1,8 +1,8 @@
 <?php
-    $hostname = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "ecom";
+    $hostname = "173.212.235.205"; // online: 173.212.235.205 , local: localhost
+    $username = "widnerco_admin"; // online: widnerco_admin , local: root
+    $password = "!CodeFactory2022"; // online: !CodeFactory2022 , local: ""
+    $dbname = "widnerco_ecom"; // online: widnerco_ecom , local: ecom
 
     $connect = mysqli_connect($hostname, $username, $password, $dbname);
 
